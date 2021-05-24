1ff6c52c4422590aada2d5e9b202ea5a7241e47d
Cesar Albuquerque <cesar.consultorjr@gmail.com>
2021-05-24 00:04:16 -0300

    Improving README by adding scripts

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Description
+
+It's project that implements automated tests against a public API: https://gorest.co.in/public-api/user
+Also it's based on a postman collection that you can find in https://www.postman.com/collections/77cfd2465c5d146503d0
+All tests have been powered on NodeJS, Cypress, Axios and FakerJS.
+If you have a question or an issue you can open an issue on this repository.
+
 # Requirements
 
 - NodeJS >= 12.0
@@ -11,4 +18,10 @@
 
 # Install dependencies
 
-- go to your favorite terminal app and run `npm install'
+- Go to your favorite terminal app and run `npm install`
+
+# How to run the test
+
+- You can open cypress by run `npm run cypress:open` and puck up each test you would like to run
+- Also, you can run all tests on terminal by run `npm run test:chrome` and see a video of execution in video directory
+
--- cypress/integration/api/challenge.spec.js
+++ cypress/integration/api/challenge.spec.js
@@ -18,96 +18,98 @@ context('API', () => {
         gender: "Male",
         status: "Active"
       };
-  
+
       const response = await axios.post(API.USERS, userData, baseConfig);
-  
+
       if (response.data.code === 201) {
         const user = response.data.data;
-  
+
         this.targetUserId = user.id;
       }
       else
         throw Error('We were not able to create a user')
     })
-  
+
     it('Check created valid user is available on list end-point', async function () {
       let targetUser;
       let pages = 1;
       let page = 1;
       const visitedPages = [];
-  
+
       // TODO: consider iterate over a filled pages array instead of decresing page number
       while (!targetUser && visitedPages.length < pages) {
         const response = await axios.get(`${API.USERS}?page=${page}`, baseConfig);
-  
+
         const users = response.data.data;
         const pagination = response.data.meta.pagination;
         const currentPage = page;
-  
+
         pages = pagination.pages;
         targetUser = users.find(u => u.id === this.targetUserId);
-  
+
         page = pages - visitedPages.length;
         visitedPages.push(currentPage);
       }
-  
+
       expect(targetUser instanceof Object).to.be.true;
     });
-  
+
     it(`Update created user's name`, async function () {
       const newName = `${faker.name.firstName()} ${faker.name.lastName()}`;
       const url = `${API.USERS}/${this.targetUserId}`;
       const userDataToBeUpdated = { name: newName };
-  
+
       const putResponse = await axios.put(url, userDataToBeUpdated, baseConfig);
       const getResponse = await axios.get(url, baseConfig);
-  
+
       expect(putResponse.status).to.equal(200);
       expect(getResponse.status).to.equal(200);
-  
+
       const updatedUser = putResponse.data.data;
       const retrievedUser = putResponse.data.data;
-  
-  
+
+
       expect(retrievedUser.name).to.equal(newName);
       expect(updatedUser).to.equal(retrievedUser);
     });
-  
+
     it('Delete created user', async function () {
       const url = `${API.USERS}/${this.targetUserId}`;
-  
+
       const deleteResponse = await axios.delete(url, baseConfig);
       const getResponse = await axios.get(url, baseConfig);
-  
+
       expect(deleteResponse.status).to.equal(200);
       expect(deleteResponse.data.code).to.equal(204);
-  
+
       expect(getResponse.status).to.equal(200);
       expect(getResponse.data.code).to.equal(404);
       expect(getResponse.data.data.message).to.equal('Resource not found');
     });
   });
 
-  it('Check that a deleted user is not listed any more', async function () {
-    const response = await axios.get(API.USERS, baseConfig);
+  context('Complementary scenarios', () => {
+    it('Check that a deleted user is not listed any more', async function () {
+      const response = await axios.get(API.USERS, baseConfig);
 
-    expect(response.status).to.equal(200);
-    expect(response.data.code).to.equal(200);
+      expect(response.status).to.equal(200);
+      expect(response.data.code).to.equal(200);
 
-    const userToBeDeleted = response.data.data[0];
-    const url = `${API.USERS}/${userToBeDeleted.id}`;
+      const userToBeDeleted = response.data.data[0];
+      const url = `${API.USERS}/${userToBeDeleted.id}`;
 
-    const deleteResponse = await axios.delete(url, baseConfig);
-    expect(deleteResponse.status).to.equal(200);
-    expect(deleteResponse.data.code).to.equal(204);
+      const deleteResponse = await axios.delete(url, baseConfig);
+      expect(deleteResponse.status).to.equal(200);
+      expect(deleteResponse.data.code).to.equal(204);
 
-    const listResponse = await axios.get(API.USERS, baseConfig);
+      const listResponse = await axios.get(API.USERS, baseConfig);
 
-    expect(response.status).to.equal(200);
-    expect(response.data.code).to.equal(200);
+      expect(response.status).to.equal(200);
+      expect(response.data.code).to.equal(200);
 
-    const notfoundUser = listResponse.data.data.find (u => u.id === userToBeDeleted.id);
+      const notfoundUser = listResponse.data.data.find(u => u.id === userToBeDeleted.id);
 
-    expect(notfoundUser).to.equal(undefined);
+      expect(notfoundUser).to.equal(undefined);
+    });
   });
 })
